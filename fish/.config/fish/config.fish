d77a6b7e41d790e224408882cf2fa002999040ab
Ian Wolterstorff <wolterstorff.i@husky.neu.edu>
2020-01-20 17:06:46 -0500

    go way

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,10 +10,17 @@ set -gx XDG_CONFIG_HOME "$HOME/.config"
 
 #if test -z "$DISPLAY"
 	set -gx JAVA_HOME /usr/lib/jvm/default
-	set -gx PATH /home/ian/.local/bin /home/ian/.gem/ruby/2.5.0/bin $HOME/bin $PATH
+	set -gx PATH /home/ian/.cargo/bin /home/ian/.local/bin /home/ian/.gem/ruby/2.5.0/bin $HOME/bin $PATH
 #end
 
 alias vim nvim
 alias byebye "sudo shutdown now"
+alias refresh-mirrors "sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist"
+alias xclip "xclip -sel clip"
+alias ghc "ghc -dynamic"
 
 cat $HOME/.config/wpg/sequences &
+
+# tabtab source for electron-forge package
+# uninstall by removing these lines or running `tabtab uninstall electron-forge`
+[ -f /home/ian/Repos/moonshot-gui/node_modules/tabtab/.completions/electron-forge.fish ]; and . /home/ian/Repos/moonshot-gui/node_modules/tabtab/.completions/electron-forge.fish
--- scripts/bin/gen-wall.sh
+++ scripts/bin/gen-wall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-magick $HOME/.logo2.png -fill "#2b303b" -fuzz 50% -opaque "#000000" -background "#65737e" -gravity center -extent 1920x1080 -flatten $HOME/.logo-bg.png
+convert -size 1920x1080 xc:#65737e $HOME/.logo-bg.png
 
 feh --bg-scale $HOME/.logo-bg.png &
